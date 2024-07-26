7f6ec6df4aa3409fbe4f3982a45bcbed2d3777e8
EunsolnCho <cho_eunsol@naver.com>
2024-07-26 21:54:24 +0900

    [feat] setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import Router from './routes/Router'
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <>
+      <RouterProvider router={Router} />
+    </>
+  );
+}
+
+export default App;
