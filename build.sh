70587cf88bcc379134538d237232c6489361cc50
EunsolnCho <cho_eunsol@naver.com>
2024-07-26 21:38:13 +0900

    [feat] setting

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./Team5-Client/* ./output
+cp -R ./output ./Team5-Client/
---
+++ src/routes/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/home';
+
+// 부모 컴포넌트로부터 로그인 여부에 대한 값 받아와서 사용
+const Router = () => {
+	const routes = [
+		{
+			path: '/',
+			element: <Home />, // 메인홈
+		}
+	];
+
+	const router = createBrowserRouter([...routes]);
+
+	// 구성요소 전달 및 활성화 -> App.jsx로 빼도 괜찮음
+	return <RouterProvider router={router} />;
+};
+
+export default Router;
