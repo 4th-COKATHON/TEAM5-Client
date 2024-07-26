890a6fce9736eb8ac640110eca40ea4bb98f4bf2
dah <cdh010126r@gmail.com>
2024-07-26 22:23:26 +0900

    [feat] react-icons

---
+++ src/pages/Bucket.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const bucket = () => {
+  return (
+    <div className='bucketContainer'>
+        <div className='friendContainer'>
+            
+        </div>
+        <div className='iceContainer'>
+
+        </div>
+    </div>
+  )
+}
+
+export default bucket
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../pages/Home';
+import Bucket from '../pages/Bucket';
 
 // 부모 컴포넌트로부터 로그인 여부에 대한 값 받아와서 사용
 const Router = () => {
-	const routes = [
-		{
-			path: '/',
-			element: <Home />, // 메인홈
-		}
-	];
+  const routes = [
+    {
+      path: '/',
+      element: <Home />, // 메인홈
+    },
+    {
+      path: '/',
+      element: <Bucket />, // 메인홈
+    },
+  ];
 
-	const router = createBrowserRouter([...routes]);
+  const router = createBrowserRouter([...routes]);
 
-	// 구성요소 전달 및 활성화 -> App.jsx로 빼도 괜찮음
-	return <RouterProvider router={router} />;
+  // 구성요소 전달 및 활성화 -> App.jsx로 빼도 괜찮음
+  return <RouterProvider router={router} />;
 };
 
 export default Router;
